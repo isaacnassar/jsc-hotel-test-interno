705c3a487bcc7e3cc0a368aecd769f98f5f68b60
Gaston F <gaston.frassatti@claro.com.ar>
2018-10-04 10:57:10 -0300

    Se agrega endopoint

--- src/main/java/com/esolutions/trainings/jsc2/web/ReservationController.java
+++ src/main/java/com/esolutions/trainings/jsc2/web/ReservationController.java
@@ -1,6 +1,7 @@
 package com.esolutions.trainings.jsc2.web;
 
 import com.esolutions.trainings.jsc2.model.Reservation;
+import com.esolutions.trainings.jsc2.model.ReservationModel;
 import com.esolutions.trainings.jsc2.model.ReservationRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,13 +16,12 @@ public class ReservationController {
     }
 
     @RequestMapping(value = "/floors/{floor}/rooms/{room}/book", method = RequestMethod.POST)
-    public Reservation getGuestNumber(
+    public ReservationModel getGuestNumber(
             @PathVariable int floor,
             @PathVariable int room,
             @RequestParam(value = "date", required = true) ReservationRequest request) {
 
 
-
-        return new Reservation(true, 0.0);
+        return new ReservationModel(true, 0.0);
     }
 }
