d146bc3354af02e00df89b375d6f13fc5e0bdcb6
Gaston F <gaston.frassatti@claro.com.ar>
2018-10-04 11:16:26 -0300

    Se agrega endopoint

--- src/main/java/com/esolutions/trainings/jsc2/web/RoomController.java
+++ src/main/java/com/esolutions/trainings/jsc2/web/RoomController.java
@@ -3,19 +3,23 @@ package com.esolutions.trainings.jsc2.web;
 import com.esolutions.trainings.jsc2.model.ReservationModel;
 import com.esolutions.trainings.jsc2.model.ReservationRequest;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RoomController {
 
-    @RequestMapping(value = "/floors/{floor}/rooms/{room}", method = RequestMethod.GET)
+    @RequestMapping(value = "/floors/{floor}/rooms/{room}/book", method = RequestMethod.POST)
     public ReservationModel reservation(
-            @PathVariable int floor, int room,
-            @RequestParam(value = "ReservationRequest", required = true) ReservationRequest request) {
+            @PathVariable int floor,
+            @PathVariable int room,
+            @RequestBody ReservationRequest request) {
 
-        return new ReservationModel(false, 0.0);
+
+
+        ReservationModel reservationModel = new ReservationModel(false, 0.0);
+        return reservationModel;
     }
 }
