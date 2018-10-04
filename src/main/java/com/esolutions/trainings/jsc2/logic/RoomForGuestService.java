898d15e3f0db33f1658b10c06b549c85c29f8aaf
Andres Giacosa <andres.giacosa@claro.com.ar>
2018-10-04 11:01:12 -0300

    Cambios para seguir con el req 1

--- src/main/java/com/esolutions/trainings/jsc2/logic/RoomForGuestService.java
+++ src/main/java/com/esolutions/trainings/jsc2/logic/RoomForGuestService.java
@@ -4,6 +4,8 @@ import com.esolutions.trainings.jsc2.model.Guest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.HashMap;
+
 public class RoomForGuestService {
 
     private final JpaRepository<Guest, Long> repository;
@@ -11,6 +13,36 @@ public class RoomForGuestService {
     @Autowired
     public RoomForGuestService(JpaRepository<Guest, Long> repository) {
         this.repository = repository;
+    }
+
+    HashMap<Integer, Integer> hotelDistribution = new HashMap<Integer, Integer>();
+
+    public void perfectSquaresRoomCalculation(int floor, int room) {
+
+        int lastFloor = 0, lastRoom = 0;
+        int guest;
+        while (lastFloor != floor && lastRoom == room) {
+
+        }
+
+    }
+
+    public HashMap<Integer, Integer> populateHotel() {
+
+
+        for (int i = 1; i <= 50000; i++) {
+
+        }
+
+        return hotelDistribution;
+    }
+
+    public boolean isPerfectSquare(double roomSum) {
+        if (Math.sqrt(roomSum) % 1 == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
